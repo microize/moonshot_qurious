a26ef1e2f92d44faea239a93c100f37eef3bd433
sripathi <3sripathi@gmail.com>
2025-04-09 19:59:49 +0530

    refactor: update dependencies and remove postcss.config.js
    - Removed @craco/craco from dependencies in package.json.
    - Updated autoprefixer, postcss, and tailwindcss versions in devDependencies.
    - Deleted postcss.config.js as it is no longer needed.
    - Reformatted tailwind.config.js for consistency and clarity.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,26 +1,26 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    darkMode: 'class', // enables dark mode with class-based approach
-    theme: {
-      extend: {
-        colors: {
-          purple: {
-            50: '#f6f5ff',
-            100: '#edebfe',
-            200: '#dcd7fe',
-            300: '#cabffd',
-            400: '#ac94fa',
-            500: '#9061f9',
-            600: '#7e3af2',
-            700: '#6c2bd9',
-            800: '#5521b5',
-            900: '#4a1d96',
-          },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          50: '#f6f5ff',
+          100: '#edebfe',
+          200: '#dcd7fe',
+          300: '#cabffd',
+          400: '#ac94fa',
+          500: '#9061f9',
+          600: '#7e3af2',
+          700: '#6c2bd9',
+          800: '#5521b5',
+          900: '#4a1d96',
         },
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
